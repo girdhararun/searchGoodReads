6fc89218cb1d4d5fb11f8dac31b6d49dab336cc9
Arun Girdhar <arungirdhar@DLLA32P3561.qait.com>
2020-08-01 23:43:23 +0530

    get rest assured

---
+++ src/main/java/GoodReadsApiCalls.java
@@ -0,0 +1,30 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import models.goodReadsSearch.GoodreadsResponse;
+import org.json.JSONObject;
+import org.json.XML;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static io.restassured.RestAssured.given;
+
+
+public class GoodReadsApiCalls {
+    String key = "11AzOEYXS2oqXaofmANuFA";
+    String secret = "PUXzY4zDsa6RzLI7ihXn4FRjQwpdtHL7DEf5CSpAI";
+
+    GoodreadsResponse searchApiObj(String searchString){
+        return given().header("Accept", "*/*")
+                .param("q", searchString).param("key",key)
+                .expect()
+                .statusCode(200)
+                .when().get("https://www.goodreads.com/search/index.xml").then().extract().body().as(GoodreadsResponse.class);
+    }
+
+    public static void main(String[] args) {
+        GoodReadsApiCalls test = new GoodReadsApiCalls();
+        System.out.println(test.searchApiObj("treasure").getSearch().getResults().getWork().get(0).getId().getValue());
+    }
+}
+
+
