a064e73aa6299128d1568d588285c2b51aaf7089
Arun Girdhar <arungirdhar@DLLA32P3561.qait.com>
2020-08-03 01:32:05 +0530

    searchMethodShouldWorkAsExpected

--- src/test/java/httprequests/GoodReadsApiCallsTest.java
+++ src/test/java/httprequests/GoodReadsApiCallsTest.java
@@ -1,8 +1,22 @@
 package httprequests;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class GoodReadsApiCallsTest {
+    GoodReadsApiCalls call = null;
+
+    @BeforeEach
+    void init(){
+        call = new GoodReadsApiCalls();
+    }
 
     @org.junit.jupiter.api.Test
-    void searchApiObj() {
+    void searchMethodShouldWorkAsExpected() {
+        int result_count = call.searchApiObj("treasure").getSearch().getTotalResults();
+        Assertions.assertEquals( 31121, result_count);
     }
+
 }
